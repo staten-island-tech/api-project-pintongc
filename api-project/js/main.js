393976210d05a86d3418102a48a46675d76f7ea4
pintongc <pintongc@nycstudents.net>
2023-12-06 09:22:16 -0500

    2

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,24 +1,17 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const URL = `'https://api-nba-v1.p.rapidapi.com/`;
 
-setupCounter(document.querySelector('#counter'))
+async function getData(URL){
+    try {
+        const response = await fetch(URL);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        document.querySelector("h1").textContent = data.content;
+        document.querySelector("h2").textContent = data.author;
+    } catch (error) {
+
+    }
+}
+getData(URL)
