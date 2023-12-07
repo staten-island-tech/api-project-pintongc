92c7f8336415c7a053ac7b55c85eb018a56f0e7f
pintongc <pintongc@nycstudents.net>
2023-12-07 09:24:26 -0500

    3

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,17 +1,18 @@
-import './style.css'
+    const URL = `https://www.balldontlie.io/api/v1/players`;
 
-const URL = `'https://api-nba-v1.p.rapidapi.com/`;
+    async function getData(URL){
+        try {
+            const response = await fetch(URL);
+            console.log(response);
+            const data = await response.json();
+            console.log(data);
+            document.querySelector("h1").textContent = data[0];
+            document.querySelector("h2").textContent = data[1];
+        } catch (error) {
 
-async function getData(URL){
-    try {
-        const response = await fetch(URL);
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
-        document.querySelector("h1").textContent = data.content;
-        document.querySelector("h2").textContent = data.author;
-    } catch (error) {
-
+        }
     }
-}
-getData(URL)
+    getData(URL)
+
+    document.querySelector(".player").textContent = getData.data;
+
