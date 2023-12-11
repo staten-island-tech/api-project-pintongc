88261d5fef74d1587848ffdd50531f7687cd4b9f
pintongc <pintongc@nycstudents.net>
2023-12-11 09:23:20 -0500

    5

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,18 +1,18 @@
-    const URL = `https://www.balldontlie.io/api/v1/season_averages?season=2023&player_ids[]=237&player_ids[]=238`;
+    const URL = `https://www.balldontlie.io/api/v1/season_averages?season=2023&player_ids[]=235&player_ids[]=236`;
 
     async function getData(URL){
         try {
             const response = await fetch(URL);
             console.log(response);
             const data = await response.json();
-            console.log(data.data);
-            document.querySelector("h1").textContent = data;
-            document.querySelector("h2").textContent = data;
+            console.log(data.data[0]);
+            document.querySelector("h1").textContent = data.data[0].pts;
+            document.querySelector("h2").textContent = data.data[0].ast;
         } catch (error) {
 
         }
     }
     getData(URL)
 
-    document.querySelector(".player").textContent = getData.data;
+    document.querySelector(".player").textContent = data.data[0].ast;
 
